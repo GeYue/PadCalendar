baf57702ae6dfb5465511c0ecce586567ea4513e
GeYue <GeYue.Me@Gmail.com>
2016-09-10 12:25:10 +0800

    Created work project, add 'PadCalendarViewController' group and reference to the real folder.

---
+++ PadCalendar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PadCalendar
+//
+//  Created by 葛岳 on 16/9/10.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PadCalendar/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PadCalendar
+//
+//  Created by 葛岳 on 16/9/10.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
