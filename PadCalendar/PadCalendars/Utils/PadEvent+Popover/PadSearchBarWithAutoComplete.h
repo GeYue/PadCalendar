17746ae66906bac82034178b81ae877cca874b99
GeYue <GeYue.Me@Gmail.com>
2016-09-26 22:20:37 +0800

    Add a little code for PadSearchBar.

---
+++ PadCalendar/PadCalendars/Utils/PadEvent+Popover/PadSearchBarWithAutoComplete.h
@@ -0,0 +1,15 @@
+//
+//  PadSearchBarWithComplete.h
+//  PadCalendar
+//
+//  Created by 葛岳 on 2016/9/26.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PadSearchBarWithComplete : UISearchBar
+
+@property (nonatomic, strong) UITableView *tableViewCustom;
+
+@end
---
+++ PadCalendar/PadCalendars/Utils/PadEvent+Popover/PadSearchBarWithAutoComplete.m
@@ -0,0 +1,19 @@
+//
+//  PadSearchBarWithComplete.m
+//  PadCalendar
+//
+//  Created by 葛岳 on 2016/9/26.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import "PadSearchBarWithComplete.h"
+
+@interface PadSearchBarWithComplete() <UITableViewDataSource, UITabBarDelegate, UISearchBarDelegate>
+
+@end
+
+
+@implementation PadSearchBarWithComplete
+
+
+@end
