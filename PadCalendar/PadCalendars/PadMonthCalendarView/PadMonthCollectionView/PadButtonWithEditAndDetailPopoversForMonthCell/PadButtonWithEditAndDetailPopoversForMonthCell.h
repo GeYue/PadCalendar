f858b5f05d599a0b654111c03ddd3b3b1602f2e4
GeYue <GeYue.Me@Gmail.com>
2016-11-22 16:31:36 -0600

    coding for PadMonthCell

---
+++ PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadButtonWithEditAndDetailPopoversForMonthCell/PadButtonWithEditAndDetailPopoversForMonthCell.h
@@ -0,0 +1,13 @@
+//
+//  PadButtonWithEditAndDetailPopoversForMonthCell.h
+//  PadCalendar
+//
+//  Created by 葛岳 on 2016/11/22.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PadButtonWithEditAndDetailPopoversForMonthCell : UIButton
+
+@end
---
+++ PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadButtonWithEditAndDetailPopoversForMonthCell/PadButtonWithEditAndDetailPopoversForMonthCell.m
@@ -0,0 +1,21 @@
+//
+//  PadButtonWithEditAndDetailPopoversForMonthCell.m
+//  PadCalendar
+//
+//  Created by 葛岳 on 2016/11/22.
+//  Copyright © 2016年 GeYue. All rights reserved.
+//
+
+#import "PadButtonWithEditAndDetailPopoversForMonthCell.h"
+
+@implementation PadButtonWithEditAndDetailPopoversForMonthCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
