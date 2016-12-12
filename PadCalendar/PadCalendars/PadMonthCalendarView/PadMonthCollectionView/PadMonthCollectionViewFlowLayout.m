1bf5e60eb275e36ed079055bb9151ebddfbc6c8b
GeYue <GeYue.Me@Gmail.com>
2016-12-12 13:57:49 -0600

    Add PadMonthCollectionViewFlowLayout class code.

--- PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadMonthCollectionViewFlowLayout.m
+++ PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadMonthCollectionViewFlowLayout.m
@@ -7,7 +7,45 @@
 //
 
 #import "PadMonthCollectionViewFlowLayout.h"
+#import "PadConstants.h"
 
 @implementation PadMonthCollectionViewFlowLayout
+
+- (CGSize) collectionViewContentSize {
+    return CGSizeMake(self.collectionView.frame.size.width, 3*(self.collectionView.frame.size.height));
+}
+
+#pragma mark - Forcing de max space between cells to be equal to SPACE_COLLECTIONVIEW_CELL
+
+- (NSArray *)layoutAttributesForElementsInRect:(CGRect)rect {
+    NSArray *arr = [super layoutAttributesForElementsInRect:rect];
+    for (UICollectionViewLayoutAttributes *atts in arr) {
+        if (nil == atts.representedElementKind){
+            NSIndexPath *ip = atts.indexPath;
+            atts.frame = [self layoutAttributesForItemAtIndexPath:ip].frame;
+        }
+    }
+    return arr;
+}
+
+- (UICollectionViewLayoutAttributes *)layoutAttributesForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewLayoutAttributes *atts = [super layoutAttributesForItemAtIndexPath:indexPath];
+    if (0 == indexPath.item || 1 == indexPath.item)
+        return atts;
+    
+    NSIndexPath *ipPrev = [NSIndexPath indexPathForItem:indexPath.item-1 inSection:indexPath.section];
+    CGRect fPrev = [self layoutAttributesForItemAtIndexPath:ipPrev].frame;
+    CGFloat rightPrev = fPrev.origin.x + fPrev.size.width + SPACE_COLLECTIONVIEW_CELL;
+    
+    if (atts.frame.origin.x <= rightPrev) {
+        return atts;
+    }
+    
+    CGRect f = atts.frame;
+    f.origin.x = rightPrev;
+    atts.frame = f;
+    
+    return atts;
+}
 
 @end
