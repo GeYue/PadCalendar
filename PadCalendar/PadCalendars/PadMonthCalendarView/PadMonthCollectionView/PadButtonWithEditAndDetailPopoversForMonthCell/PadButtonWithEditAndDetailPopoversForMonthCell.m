5f66d593282c9da2a09203b377fa3891eb32a365
GeYue <GeYue.Me@Gmail.com>
2016-12-14 21:05:52 -0600

    implementation of PadEventPopoverView part II.

--- PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadButtonWithEditAndDetailPopoversForMonthCell/PadButtonWithEditAndDetailPopoversForMonthCell.m
+++ PadCalendar/PadCalendars/PadMonthCalendarView/PadMonthCollectionView/PadButtonWithEditAndDetailPopoversForMonthCell/PadButtonWithEditAndDetailPopoversForMonthCell.m
@@ -8,8 +8,49 @@
 
 #import "PadButtonWithEditAndDetailPopoversForMonthCell.h"
 
+#import "PadEventContentPopoverController.h"
+
+@interface PadButtonWithEditAndDetailPopoversForMonthCell() <PadEventContentPopoverControllerProtocol>
+@property (nonatomic, strong) PadEventContentPopoverController *popoverContentController;
+
+@end
 
 @implementation PadButtonWithEditAndDetailPopoversForMonthCell
 
+#pragma mark - Lifecycle
+
+-(id) initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self){
+        [self setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        [self.titleLabel setFont:[UIFont systemFontOfSize:12.]];
+        [self.titleLabel setTextAlignment:NSTextAlignmentLeft];
+        
+        [self addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return self;
+}
+
+- (void) setEvent:(PadEvent *)event {
+    _event = event;
+    [self setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+}
+
+#pragma mark - Button Action
+
+- (IBAction)buttonAction:(id)sender {
+    if (_event) {
+        _popoverContentController = [[PadEventContentPopoverController alloc] initWithEvent:_event];
+        [_popoverContentController setProtocol:self];
+        
+        
+    }
+}
+
+#pragma mark - PadEventContentPopoverController Protocol
+
+- (void) showPopoverEditWithEvent:(PadEvent *)event {
+    
+}
 
 @end
--- PadCalendar/PadCalendars/Utils/PadEventContent+Popover/PadEventContentView.m
+++ PadCalendar/PadCalendars/Utils/PadEventContent+Popover/PadEventContentView.m
@@ -56,20 +56,53 @@
 #pragma mark - Add Subviews
 
 - (void) addButtonEditPopoverWithViewSize:(CGSize)sizeView {
-
+    CGFloat width = 50;
+    CGFloat height = BUTTON_HEIGHT;
+    CGFloat gap = 30;
     
+    _buttonEditPopover = [[UIButton alloc] initWithFrame:CGRectMake(sizeView.width-width-gap, 22, width, height)];
+    [_buttonEditPopover setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [_buttonEditPopover setTitle:@"Edit" forState:UIControlStateNormal];
+    [_buttonEditPopover.titleLabel setFont:[UIFont boldSystemFontOfSize:_buttonEditPopover.titleLabel.font.pointSize]];
+    [_buttonEditPopover addTarget:self action:@selector(buttonEditPopoverAction:) forControlEvents:UIControlEventTouchUpInside];
+    [_buttonEditPopover setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin];
+    
+    [self addSubview:_buttonEditPopover];
 }
 
 - (void) addLabelCustomnameWithViewSize:(CGSize)sizeView {
+    CGFloat gap = 30;
     
+    _labelCustomName = [[UILabel alloc] initWithFrame:CGRectMake(gap, _buttonEditPopover.frame.origin.y, sizeView.width-3*gap-_buttonEditPopover.frame.size.width, _buttonEditPopover.frame.size.height)];
+    [_labelCustomName setText:_event.stringEventName];
+    [_labelCustomName setFont:[UIFont boldSystemFontOfSize:_labelCustomName.font.pointSize]];
+    
+    [self addSubview:_labelCustomName];
 }
 
 - (void) addLabelDateWithViewSize:(CGSize)sizeView {
+    CGFloat gap = 30;
+    _labelDate = [[UILabel alloc] initWithFrame:CGRectMake(gap, _labelCustomName.frame.origin.y+_labelCustomName.frame.size.height, 180, _labelCustomName.frame.size.height)];
+    [_labelDate setText:[NSDate stringDayOfDate:_event.dateDay]];
+    [_labelDate setTextColor:[UIColor grayColor]];
+    [_labelDate setFont:[UIFont systemFontOfSize:_labelCustomName.font.pointSize-3]];
     
+    [self addSubview:_labelDate];
 }
 
 - (void) addLabelTimeWithViewSize:(CGSize)sizeView {
+    CGFloat gap = 30;
+    CGFloat x = _labelDate.frame.origin.x + _labelDate.frame.size.width + gap;
     
+    _labelTime = [[UILabel alloc] initWithFrame:CGRectMake(x, _labelDate.frame.origin.y
+                                                           , sizeView.width-x-gap, _labelDate.frame.size.height)];
+    [_labelTime setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin];
+    [_labelTime setText:[NSString stringWithFormat:@"%@ - %@", [NSDate stringTimeOfDate:_event.dateTimeBegin], [NSDate stringTimeOfDate:_event.dateTimeEnd]]];
+    [_labelTime setTextAlignment:NSTextAlignmentRight];
+    [_labelTime setTextColor:[UIColor grayColor]];
+    [_labelTime setFont:_labelDate.font];
+    
+    [self addSubview:_labelTime];
 }
 
 @end
